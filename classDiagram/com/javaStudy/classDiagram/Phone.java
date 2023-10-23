b0799c4d734483095b9ba6b522614feb6bbae231
JunHoJang <jjjh3369@naver.com>
2023-10-23 10:01:46 +0900

    클래스 다이어그램 수정

--- classDiagram/com/javaStudy/classDiagram/Phone.java
+++ classDiagram/com/javaStudy/classDiagram/Phone.java
@@ -2,13 +2,4 @@ package com.javaStudy.classDiagram;
 
 public class Phone {
 
-	private int phones;
-
-	public int getPhones() {
-		return phones;
-	}
-
-	public void setPhones(int phones) {
-		this.phones = phones;
-	}
 }
